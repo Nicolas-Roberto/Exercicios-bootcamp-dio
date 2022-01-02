b928f556f36b725e07a1ddedd58a0c8f32ba8c07
Nicolas-Roberto <nicolas.roberto987@gmail.com>
2022-01-02 17:12:41 -0300

    6_CalculadoraDeNotas

--- 6_CalculadoraDeNotas/app/src/main/java/com/example/a6_calculadoradenotas/MainActivity.kt
+++ 6_CalculadoraDeNotas/app/src/main/java/com/example/a6_calculadoradenotas/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.a6_calculadoradenotas
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCalcular = btnCalcular
+        val resultado = resultado
+
+        btnCalcular.setOnClickListener {
+            val nota1 = Integer.parseInt(nota_1.text.toString())
+            val nota2 = Integer.parseInt(nota_2.text.toString())
+            val nota3 = Integer.parseInt(nota_3.text.toString())
+            val nota4 = Integer.parseInt(nota_4.text.toString())
+            val faltas = Integer.parseInt(faltas.text.toString())
+
+            val notaFinal = (nota1 + nota2 + nota3 + nota4) / 4
+
+            if(notaFinal >= 6 && faltas <= 25) {
+                resultado.text = "Aluno aprovado \n Nota Final: $notaFinal \n Faltas: $faltas"
+                resultado.setTextColor(Color.GREEN)
+            } else {
+                resultado.text = "Aluno reprovado \n Nota Final: $notaFinal \n Faltas: $faltas"
+                resultado.setTextColor(Color.RED)
+            }
+        }
     }
 }
